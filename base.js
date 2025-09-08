ac4ef026e1581f742d0fc6e5a85e938a33371d95
AH <allie@crit.rip>
2025-09-07 20:37:19 -0400

    init

---
+++ base.js
@@ -0,0 +1,15 @@
+function drawCanvas() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(10, 10, 150, 100);
+}
+
+/*
+
+*/
+
+
+/*
+
+*/
